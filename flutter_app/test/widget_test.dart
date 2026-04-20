1ee07418fc3f0db3625df49d050e464a18a4cb66
wanxt <smartv@qq.com>
2026-04-20 09:55:25 +0800

    认证单元测试

--- flutter_app/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App smoke test placeholder', (WidgetTester tester) async {
-    // TODO: Replace with proper widget test once DI is set up for testing.
-    expect(1 + 1, 2);
-  });
-}
